d0dffd8b9e855c4ed217799a2671e607226f29b0
Nemanja <nemanja.vasic@codecentric.de>
2019-10-20 00:37:54 +0200

    Delete commented code

--- server.js
+++ server.js
@@ -5,29 +5,6 @@ const { PORT } = require("./config");
 
 const app = express();
 
-// const Twit = require("twit");
-// const {
-//   CONSUMER_KEY,
-//   CONSUMER_SECRET,
-//   ACCESS_TOKEN,
-//   ACCESS_TOKEN_SECRET
-// } = require("./config");
-
-// const twit = new Twit({
-//   consumer_key: CONSUMER_KEY,
-//   consumer_secret: CONSUMER_SECRET,
-//   access_token: ACCESS_TOKEN,
-//   access_token_secret: ACCESS_TOKEN_SECRET
-// });
-
-// twit.get(
-//   "friends/list",
-//   { screen_name: "Nemanjas_Vasic" },
-//   (err, data, response) => {
-//     console.log(data);
-//   }
-// );
-
 app.use(
   "/graphql",
   graphqlHTTP({
