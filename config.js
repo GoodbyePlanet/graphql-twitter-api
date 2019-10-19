ce215335c9b619f4152e498a9241f10b414a6ad4
Nemanja <nemanja.vasic@codecentric.de>
2019-10-19 22:22:59 +0200

    Add Twitter API configuration

--- config.js
+++ config.js
@@ -11,5 +11,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
+  CONSUMER_KEY: process.env.CONSUMER_KEY,
+  CONSUMER_SECRET: process.env.CONSUMER_SECRET,
+  ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET
 };
--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const graphqlSchema = require("./schema");
-const { PORT } = require("./config");
+const schema = require("./schema");
+const {
+  PORT,
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+  ACCESS_TOKEN,
+  ACCESS_TOKEN_SECRET
+} = require("./config");
+
+const Twit = require("twit");
+
+const twit = new Twit({
+  consumer_key: CONSUMER_KEY,
+  consumer_secret: CONSUMER_SECRET,
+  access_token: ACCESS_TOKEN,
+  access_token_secret: ACCESS_TOKEN_SECRET
+});
+
+twit.get(
+  "users/lookup",
+  { screen_name: "Nemanjas_Vasic" },
+  (err, data, response) => {
+    console.log(data);
+    // console.log("Number of followers", data.ids.length);
+  }
+);
 
 const app = express();
 
-app.listen(PORT, () => {
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(PORT || 4001, () => {
   console.log("Server running on PORT", PORT);
 });
