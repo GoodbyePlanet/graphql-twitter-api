22a02147518899d7bdbc86e2c4ca5daa03ca5e7e
Nemanja <nemanja.vasic@codecentric.de>
2019-11-10 16:13:32 +0100

    Handle promise error

--- model/TwitterUser.js
+++ model/TwitterUser.js
@@ -3,34 +3,33 @@ const twit = require("../config/twit");
 const getUser = screen_name => {
   return twit
     .get("users/lookup", { screen_name })
-    .catch(err => handleError(err))
     .then(({ data }) => ({
       name: data[0].name,
       screen_name: data[0].screen_name,
       description: data[0].description,
       followers_count: data[0].followers_count,
       friends_count: data[0].friends_count
-    }));
+    }))
+    .catch(err => handleError(err));
 };
 
 const getUserFriends = screen_name => {
   return twit
     .get("friends/list", { screen_name })
-    .catch(err => handleError(err))
-    .then(({ data: { users: friends } }) => ({ friends }));
+    .then(({ data: { users: friends } }) => ({ friends }))
+    .catch(err => handleError(err));
 };
 
 const getUserFollowers = screen_name => {
   return twit
     .get("followers/list", { screen_name })
-    .catch(err => handleError(err))
-    .then(({ data: { users: followers } }) => ({ followers }));
+    .then(({ data: { users: followers } }) => ({ followers }))
+    .catch(err => handleError(err));
 };
 
 const getUserTweets = screen_name => {
   return twit
     .get("statuses/user_timeline", { screen_name })
-    .catch(err => handleError(err))
     .then(({ data }) => {
       return data.map(tweet => ({
         created_at: tweet.created_at,
@@ -38,11 +37,13 @@ const getUserTweets = screen_name => {
         retweets_count: tweet.retweet_count,
         likes: tweet.favorite_count
       }));
-    });
+    })
+    .catch(err => handleError(err));
 };
 
 const handleError = error => {
   console.log("An error occured", error);
+  return new Error("Whooops!", error);
 };
 
 module.exports = {
